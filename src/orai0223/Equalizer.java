d4ddbe734a348e297e6911aba381903dbfbae26b
KeresztesHunor <kereszteshunor36gmail.com>
2023-02-23 16:22:39 +0100

    eq metódus

--- src/orai0223/Equalizer.java
+++ src/orai0223/Equalizer.java
@@ -1,9 +1,29 @@
 package orai0223;
 
+import java.util.Random;
+
 public class Equalizer
 {
+    static Random rnd = new Random();
+    
     public static void main(String[] args)
     {
-        
+        eq();
+        eq();
+        eq();
+        eq();
+        eq();
+    }
+    
+    static void eq()
+    {
+        int also = 3;
+        int felso = 7;
+        int db = rnd.nextInt(felso - also + 1) + also;
+        for (int i = 0; i < db; i++)
+        {
+            System.out.print("\u001B[45m" + " ");
+        }
+        System.out.println("");
     }
 }
